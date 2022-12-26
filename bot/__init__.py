0f26be9936b5fb122525078fb8a0750e88d81f64
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2022-12-26 23:55:35 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,9 +3,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-API_ID = int(os.environ.get("ID" "977080"))
-API_HASH = str(os.environ.get("HASH" "0c20c4265501492a1513f91755acd42b"))
-BOT_TOKEN = str(os.environ.get("TOKEN" "5733427546:AAGmOgnJmgdi8Gi0TLBg8pnzFCfKGApR-F0"))
+API_ID = int(os.environ.get("ID"," 977080"))
+API_HASH = str(os.environ.get("HASH", "0c20c4265501492a1513f91755acd42b"))
+BOT_TOKEN = str(os.environ.get("TOKEN", "5733427546:AAGmOgnJmgdi8Gi0TLBg8pnzFCfKGApR-F0"))
 
 GDTOT_CRYPT = str(os.environ.get("GDTOT_CRYPT", "SIE1alhON191TytQc212Q1JVMVRBTG52aGCbDhE+U8xbEVLVWRObk30D0"))
 APPDRIVE_EMAIL = str(os.environ.get("APPDRIVE_EMAIL", ""))
